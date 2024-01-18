2b89c35127f15c621a0574396897712cde3e509f
Youssef Meskini <youss.meskini@gmail.com>
2024-01-18 10:50:54 +0100

    run upload in parrallel

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import io
 import os
 from datetime import datetime
 import logging
+import concurrent.futures
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -23,18 +24,21 @@ s3 = boto3.client(
   region_name = "auto",
 )
 
+def upload_image_to_s3(image, fname):
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, format='JPEG')
+    image_bytes.seek(0)
+    s3.upload_fileobj(image_bytes, BUCKET, fname)
+    image.close()
+    logging.info(f'Uploaded {fname}')
 
 def convert_pdf_to_jpg(response, author, bookId):
     with tempfile.TemporaryDirectory() as path:
         images_from_path = convert_from_bytes(response, output_folder=path)
-        for i, image in enumerate(images_from_path):
-            fname = f'books/{author}/{bookId}/{i}.jpeg'
-            image_bytes = io.BytesIO()
-            image.save(image_bytes, format='JPEG')
-            image_bytes.seek(0)
-            s3.upload_fileobj(image_bytes, BUCKET, fname)
-            image.close()
-            logging.info(f'Uploaded {fname}')
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for i, image in enumerate(images_from_path):
+                fname = f'books/{author}/{bookId}/{i}.jpeg'
+                executor.submit(upload_image_to_s3, image, fname)
 
 @app.post("/books")
 def read_root(url: str, author: str, bookId: str, background_tasks: BackgroundTasks):
